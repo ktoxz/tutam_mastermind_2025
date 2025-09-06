32e72a60e6500bc80f80b3a481e3a05e7b2b82f9
KhangKoii <hpgiakhang.personal@gmail.com>
2025-09-06 17:10:33 +0700

    refactor(emosense): move emosense from fastapi to expressjs

--- apps/be/src/routes/v1/index.js
+++ apps/be/src/routes/v1/index.js
@@ -4,6 +4,7 @@ import { StatusCodes } from 'http-status-codes'
 import { authRoute } from './authRoute.js'
 import { userRoute } from './userRoute.js'
 import { uploadRoute } from './uploadRoute.js'
+import { predictRoute } from './predictRoute.js'
 
 const Router = express.Router()
 
@@ -14,5 +15,6 @@ Router.get('/', (req, res) => {
 Router.use('/auth', authRoute)
 Router.use('/user', userRoute)
 Router.use('/upload', uploadRoute)
+Router.use('/predict', predictRoute)
 
 export const APIs_V1 = Router
--- apps/fe/src/utils/http-client.util.ts
+++ apps/fe/src/utils/http-client.util.ts
@@ -4,60 +4,51 @@ import { AuthService } from '@/services/api/auth/auth.service';
 import { lockProcess } from './process-lock.util';
 
 export const BACKEND_API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:5000/api/v1';
-export const MODEL_API_URL = process.env.NEXT_PUBLIC_MODEL_API_URL || 'http://localhost:8000';
 
 const httpClient = axios.create({
-	withCredentials: true,
-	baseURL: BACKEND_API_URL,
-	timeout: 10000,
-	headers: {
-		'Content-Type': 'application/json',
-	},
-});
-
-const modelHttpClient = axios.create({
-	baseURL: MODEL_API_URL,
-	timeout: 10000,
-	headers: {
-		'Content-Type': 'application/json',
-	},
+    withCredentials: true,
+    baseURL: BACKEND_API_URL,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 httpClient.interceptors.request.use((config) => {
-	const token = LocalStorageService.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
-	if (token && config.headers) {
-		config.headers['Authorization'] = `Bearer ${token}`;
-	}
-	return config;
+    const token = LocalStorageService.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+    if (token && config.headers) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
 });
 
 httpClient.interceptors.response.use(undefined, async (error: AxiosError) => {
-	if (error.response && error.response.status === 403) {
-		const originalRequest = error.config;
+    if (error.response && error.response.status === 403) {
+        const originalRequest = error.config;
 
-		if (originalRequest && !(originalRequest as any)._retry) {
-			(originalRequest as any)._retry = true;
+        if (originalRequest && !(originalRequest as any)._retry) {
+            (originalRequest as any)._retry = true;
 
-			const [lockError, refreshResult] = await lockProcess('REFRESH_TOKEN_LOCK', async () => {
-				return await AuthService.getInstance().refreshToken();
-			});
+            const [lockError, refreshResult] = await lockProcess('REFRESH_TOKEN_LOCK', async () => {
+                return await AuthService.getInstance().refreshToken();
+            });
 
-			const [refreshError, refreshData] = refreshResult || [null, null];
+            const [refreshError, refreshData] = refreshResult || [null, null];
 
-			if (!lockError && !refreshError && refreshData?.accessToken) {
-				LocalStorageService.setItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN, refreshData.accessToken);
-				if (originalRequest.headers) {
-					originalRequest.headers['Authorization'] = `Bearer ${refreshData.accessToken}`;
-				}
-				return httpClient(originalRequest);
-			} else {
-				LocalStorageService.removeItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
-			}
-		}
-	}
+            if (!lockError && !refreshError && refreshData?.accessToken) {
+                LocalStorageService.setItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN, refreshData.accessToken);
+                if (originalRequest.headers) {
+                    originalRequest.headers['Authorization'] = `Bearer ${refreshData.accessToken}`;
+                }
+                return httpClient(originalRequest);
+            } else {
+                LocalStorageService.removeItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+            }
+        }
+    }
 
-	error.message = (error.response?.data as any)?.message || '';
-	return Promise.reject(error);
+    error.message = (error.response?.data as any)?.message || '';
+    return Promise.reject(error);
 });
 
-export { httpClient, modelHttpClient };
+export { httpClient };
